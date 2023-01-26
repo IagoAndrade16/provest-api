d797283bc408f96023032b0f43c87beb1f8477c4
Iago Alexandre <iagoaap@outlook.com>
2023-01-26 16:29:42 -0300

    change project name

--- src/infra/database/index.ts
+++ src/infra/database/index.ts
@@ -1,12 +1,11 @@
-import { DataSource } from "typeorm";
+// import { DataSource } from "typeorm";
 
-const dataSource = new DataSource({
-  type: "postgres",
-  host: "fit_api_database",
-  username: "pedro",
-  port: 5432,
-  password: "senhasupersecreta",
-  database: "fitAPI",
-});
-
-dataSource.initialize();
+// const dataSource = new DataSource({
+//   type: "postgres",
+//   host: "fit_api_database",
+//   username: "pedro",
+//   port: 5432,
+//   password: "senhasupersecreta",
+//   database: "fitAPI",
+// });
+// dataSource.initialize();
