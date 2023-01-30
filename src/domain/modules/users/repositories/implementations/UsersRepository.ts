aa794d03ade1ec62466c948e82836397f7d585e1
Iago Alexandre <iagoaap@outlook.com>
2023-01-30 16:15:07 -0300

    email verification added

--- src/domain/modules/users/repositories/implementations/UsersRepository.ts
+++ src/domain/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,6 +14,12 @@ class UsersRepository implements IUsersRepository {
 
     await this.repository.save(user);
   }
+
+  async findByEmail(email: string): Promise<User | undefined> {
+    const user = await this.repository.findOne({ email });
+
+    return user;
+  }
 }
 
 export { UsersRepository };
