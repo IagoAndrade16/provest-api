37ff2daf69d8bd86a685f9930d0953e6ea982fab
Iago Alexandre <iagoaap@outlook.com>
2023-02-06 06:36:15 -0300

    create course tests

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,10 @@
  * https://jestjs.io/docs/configuration
  */
 
+import { pathsToModuleNameMapper } from "ts-jest";
+
+import { compilerOptions } from "./tsconfig.json";
+
 export default {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
@@ -88,7 +92,9 @@ export default {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: "<rootDir>/src/",
+  }),
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
