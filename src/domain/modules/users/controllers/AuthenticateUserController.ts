3f9541a386ab1a178b131681c3a54c3298426123
Iago Alexandre <iagoaap@outlook.com>
2023-05-03 22:18:47 -0300

    added yup validation auth user controller

--- src/domain/modules/users/controllers/AuthenticateUserController.ts
+++ src/domain/modules/users/controllers/AuthenticateUserController.ts
@@ -1,11 +1,18 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
+import * as yup from "yup";
 
 import { AuthenticateUserUseCase } from "../useCases/AuthenticateUserUseCase";
 
+const bodySchema = yup.object().shape({
+  email: yup.string().email().required().max(64),
+  password: yup.string().required().max(20),
+});
 class AuthenticateUserController {
   async handle(req: Request, res: Response): Promise<Response> {
-    const { email, password } = req.body;
+    const { email, password } = await bodySchema.validate(req.body, {
+      abortEarly: false,
+    });
 
     const authenticateUserUseCase = container.resolve(AuthenticateUserUseCase);
 
