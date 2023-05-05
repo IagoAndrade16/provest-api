45b57b0d0dccfe47c1cf1171b7862d091c15b9bf
Iago Alexandre <iagoaap@outlook.com>
2023-05-05 05:33:48 -0300

    auth user controller tests

--- src/domain/modules/users/controllers/__tests__/AuthenticateUserController.spec.ts
+++ src/domain/modules/users/controllers/__tests__/AuthenticateUserController.spec.ts
@@ -6,27 +6,78 @@ import { v4 as uuidV4 } from "uuid";
 
 let connection: Connection;
 
-describe("Auth user", () => {
-  beforeAll(async () => {
-    connection = await createConnection();
-    await connection.runMigrations();
+const route = "/users/session";
 
-    const id = uuidV4();
-    const password = await hash("admin", 8);
+beforeAll(async () => {
+  connection = await createConnection();
+  await connection.runMigrations();
 
-    await connection.query(
-      `INSERT INTO USERS(id, name, email, password, created_at, updated_at)
-        values('${id}', 'Controller', 'admin@provest.com.br', '${password}', 'now()', 'now()')
-      `
-    );
+  const id = uuidV4();
+  const password = await hash("admin", 8);
+
+  await connection.query(
+    `INSERT INTO USERS(id, name, email, password, created_at, updated_at)
+      values('${id}', 'Controller', 'admin@provest.com.br', '${password}', 'now()', 'now()')
+    `
+  );
+});
+
+afterAll(async () => {
+  await connection.dropDatabase();
+  await connection.close();
+});
+
+describe("Schema validation", () => {
+  it("should required necessary parameters", async () => {
+    const response = await request(app).post(route).send({});
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty("email");
+    expect(response.body).toHaveProperty("password");
   });
 
-  afterAll(async () => {
-    await connection.dropDatabase();
-    await connection.close();
+  describe("email", () => {
+    it("should require a valid email", async () => {
+      const response = await request(app).post(route).send({
+        email: "invalid email",
+        password: "123456",
+      });
+
+      expect(response.status).toBe(400);
+      expect(response.body).toHaveProperty("email");
+    });
+
+    it("should require a valid email length", async () => {
+      const response = await request(app)
+        .post(route)
+        .send({
+          email: `iago${"a".repeat(255)}@gmail.com`,
+          password: "123456",
+        });
+
+      expect(response.status).toBe(400);
+      expect(response.body).toHaveProperty("email");
+    });
   });
+
+  describe("password", () => {
+    it("should require a valid password length", async () => {
+      const response = await request(app)
+        .post(route)
+        .send({
+          email: "admin@provest.com.br",
+          password: "a".repeat(21),
+        });
+
+      expect(response.status).toBe(400);
+      expect(response.body).toHaveProperty("password");
+    });
+  });
+});
+
+describe("Return values", () => {
   it("should be able to authenticate user", async () => {
-    const response = await request(app).post("/users/session").send({
+    const response = await request(app).post(route).send({
       email: "admin@provest.com.br",
       password: "admin",
     });
