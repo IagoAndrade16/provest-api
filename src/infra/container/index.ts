eb5afd3d0d4a5d3f8205c7b476260afab88f9f99
Iago Alexandre <iagoaap@outlook.com>
2023-05-08 06:20:38 -0300

    test utils tests

--- src/infra/container/index.ts
+++ src/infra/container/index.ts
@@ -1,18 +1,24 @@
 import { JwtProviderImpl } from "@infra/providers/implementations/JwtProviderImpl";
 import { IJwtProvider, JwtProviderAlias } from "@infra/providers/JwtProvider";
-import { ICoursesRepository } from "@modules/courses/repositories/ICoursesRepository";
+import {
+  coursesRepositoryAlias,
+  ICoursesRepository,
+} from "@modules/courses/repositories/ICoursesRepository";
 import { CoursesRepository } from "@modules/courses/repositories/implementations/CoursesRepository";
 import { UsersRepository } from "@modules/users/repositories/implementations/UsersRepository";
-import { IUsersRepository } from "@modules/users/repositories/IUsersRepository";
+import {
+  IUsersRepository,
+  usersRepositoryAlias,
+} from "@modules/users/repositories/IUsersRepository";
 import { container } from "tsyringe";
 
 container.registerSingleton<IUsersRepository>(
-  "UsersRepository",
+  usersRepositoryAlias,
   UsersRepository
 );
 
 container.registerSingleton<ICoursesRepository>(
-  "CoursesRepository",
+  coursesRepositoryAlias,
   CoursesRepository
 );
 
